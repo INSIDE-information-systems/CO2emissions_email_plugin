7ac620ba0d83caa562e8beeda7f80d1d158c6710
Harfeur <maxou3153@gmail.com>
2021-06-30 20:41:41 +0200

    Ajout taille PJ + Slider affichage par destinataire

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,68 +1,87 @@
-const HEADER_SIZE = 800;
-// Pour 1 Mo
-const CO2 = 20;
-const OIL = 6;
-const CAR = 150;
-const MO = 1048576;
-
-function onError(error) {
-    console.log(`Error: ${error}`);
+function docReady(fn) {
+    document.addEventListener("DOMContentLoaded", fn);
 }
 
-function lengthInUtf8Bytes(str) {
-    return (new TextEncoder().encode(str)).length;
-}
+docReady(function () {
+    const HEADER_SIZE = 800;
+    // Pour 1 Mo
+    const CO2 = 20;
+    const OIL = 6;
+    const CAR = 150;
+    const MO = 1048576;
 
-function formatBytes(bytes, decimals = 2) {
-    if (bytes < 2) return bytes.toFixed(decimals) + ' octet';
+    let tab;
 
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ['octets', 'ko', 'Mo', 'Go'];
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    function lengthInUtf8Bytes(str) {
+        return (new TextEncoder().encode(str)).length;
+    }
 
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-}
+    function formatBytes(bytes, decimals = 2) {
+        if (bytes === 0) return "0 octet";
+        if (bytes < 2) return bytes.toFixed(decimals) + ' octet';
 
-function formatGrammes(size, decimals = 2) {
-    if (size < 2) return size.toFixed(decimals) + " g";
+        const k = 1024;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ['octets', 'ko', 'Mo', 'Go'];
 
-    const k = 1000;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ['g', 'kg', 't'];
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-    const i = Math.floor(Math.log(size) / Math.log(k));
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    }
 
-    return parseFloat((size / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-}
+    function formatGrammes(size, decimals = 2) {
+        if (size < 2) return size.toFixed(decimals) + " g";
 
-var gettingCurrent = browser.tabs.query({currentWindow: true});
-gettingCurrent.then(async tabInfo => {
-    // console.log(tabInfo);
-    var data = await messenger.compose.getComposeDetails(tabInfo[0].id);
-    var attachments = await messenger.compose.listAttachments(tabInfo[0].id);
+        const k = 1000;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ['g', 'kg', 't'];
 
-    let messageBodySize = lengthInUtf8Bytes(data.body);
-    let to = data.to;
-    let cc = data.cc;
-    let bcc = data.bcc;
-    let recipentsCount = to.length + cc.length + bcc.length;
-    let headerSize = HEADER_SIZE + lengthInUtf8Bytes(to.join(",") + cc.join(",") + bcc.join(","));
-    let attachmentsSize = attachments.reduce((acc, val) => acc + val.size, 0);
+        const i = Math.floor(Math.log(size) / Math.log(k));
 
-    document.getElementById("header-size").innerText = formatBytes(headerSize);
-    document.getElementById("body-size").innerText = formatBytes(messageBodySize);
-    document.getElementById("recipents-count").innerText = recipentsCount;
+        return parseFloat((size / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    }
 
-    let totalSize = headerSize + messageBodySize + attachmentsSize;
-    let sizeTimesRecipents = totalSize * recipentsCount;
-    let co2 = (sizeTimesRecipents * CO2) / MO;
-    let petrole = (sizeTimesRecipents * OIL) / MO;
-    let voiture = (sizeTimesRecipents * CAR) / MO;
+    let gettingCurrent = browser.tabs.query({currentWindow: true});
 
-    document.getElementById("size").innerText = formatBytes(totalSize);
-    document.getElementById("co2").innerText = formatGrammes(co2);
-    document.getElementById("oil").innerText = formatGrammes(petrole);
-    document.getElementById("car").innerText = voiture.toFixed(0) + " km";
-}, onError);
+    async function calculate(tabInfo) {
+        tab = tabInfo;
+
+        // console.log(tabInfo);
+        var data = await messenger.compose.getComposeDetails(tabInfo[0].id);
+        var attachments = await messenger.compose.listAttachments(tabInfo[0].id);
+
+        let messageBodySize = lengthInUtf8Bytes(data.body);
+        let to = data.to;
+        let cc = data.cc;
+        let bcc = data.bcc;
+        let subject = data.subject;
+        let recipentsCount = to.length + cc.length + bcc.length;
+        let headerSize = HEADER_SIZE + lengthInUtf8Bytes(to.join(",") + cc.join(",") + bcc.join(",") + subject);
+        let attachmentsSize = attachments.reduce((acc, val) => acc + val.size, 0);
+
+        document.getElementById("header-size").innerText = formatBytes(headerSize);
+        document.getElementById("body-size").innerText = formatBytes(messageBodySize)
+        document.getElementById("attachments-size").innerText = formatBytes(attachmentsSize);
+
+        let totalSize = headerSize + messageBodySize + attachmentsSize;
+        let sizeTimesRecipents = document.getElementById("check_values").checked ? totalSize * recipentsCount : totalSize;
+        let co2 = (sizeTimesRecipents * CO2) / MO;
+        let petrole = (sizeTimesRecipents * OIL) / MO;
+        let voiture = (sizeTimesRecipents * CAR) / MO;
+
+        document.getElementById("size").innerText = formatBytes(totalSize);
+        document.getElementById("co2").innerText = formatGrammes(co2);
+        document.getElementById("oil").innerText = formatGrammes(petrole);
+        document.getElementById("car").innerText = voiture.toFixed(0) + " km";
+    }
+
+    gettingCurrent.then(calculate, onError);
+
+    document.getElementById("check_values").addEventListener('change', () => {
+        calculate(tab);
+    });
+});
