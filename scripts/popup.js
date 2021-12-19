e950b05465ab89e48ecaf8455b1745936b3d6b4a
Baazinga <alexandre.couvez@hotmail.com>
2021-12-19 13:45:29 +0000

    Code plus compréhensible

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,12 +1,4 @@
 /**
- * Lance une fonction quand le document est chargé
- * @param {function} fn 
- */
-function docReady(fn) {
-    document.addEventListener("DOMContentLoaded", fn);
-}
-
-/**
  * Ajoute la fonction format() (comme Python) dans JS
  * @returns {String}
  */
@@ -121,158 +113,166 @@ function formatTime(time, decimals = 1) {
 }
 
 
+const HEADER_SIZE = 800;
+// Pour 1 Mo
+const CO2 = 13; // en g/Mo
+const CO2u = 6; // en g/Mo
 
+const OIL = 3.34; // en g CO2e/g de pétrole
+const CAR = 0.192; // en g CO2e/m
+const TGV = 1.73e-3; // en g CO2e/m
+const BULB = 0.0599 / 60; // en g CO2e/(W.min) (électricité)
+const BULBW = 40; // en W
+const BREATHING = 1.131; // en g CO2/min
+const MO = 1048576;
 
+let recipientsCount, totalSize, co2, petrole, voiture, tgv, ampoule, respiration; // variables globales
 
-
-docReady(function () {
-    const HEADER_SIZE = 800;
-    // Pour 1 Mo
-    const CO2 = 13; // en g/Mo
-    const CO2u = 6; // en g/Mo
-
-    const OIL = 3.34; // en g CO2e/g de pétrole
-    const CAR = 0.192; // en g CO2e/m
-    const TGV = 1.73e-3; // en g CO2e/m
-    const BULB = 0.0599 / 60; // en g CO2e/(W.min) (électricité)
-    const BULBW = 40; // en W
-    const BREATHING = 1.131; // en g CO2/min
-    const MO = 1048576;
-
-    let recipientsCount, totalSize, co2, petrole, voiture, tgv, ampoule, respiration; // variables globales
-
-    // Lancement de la promesse pour obtenir les tabs
-    let gettingCurrent = browser.tabs.query({currentWindow: true});
-
-    /**
+/**
      * Calcule l'impact d'un mail
      * @param {Object} tabInfo Information sur les onglets
      */
-    async function calculate(tabInfo) {
-        // Récupération des informations sur le mail
-        var data = await messenger.compose.getComposeDetails(tabInfo[0].id);
-        var attachments = await messenger.compose.listAttachments(tabInfo[0].id);
+ async function calculate(tabInfo) {
+    // Récupération des informations sur le mail
+    var data = await messenger.compose.getComposeDetails(tabInfo[0].id);
+    var attachments = await messenger.compose.listAttachments(tabInfo[0].id);
 
-        // Rangement de chaque information dans une variable et calcul des différentes tailles en octets
-        let messageBodySize = lengthInUtf8Bytes(data.isPlainText ? data.plainTextBody : data.body);
-        let to = data.to;
-        let cc = data.cc;
-        let bcc = data.bcc;
-        let subject = data.subject;
-        recipientsCount = to.length + cc.length + bcc.length;
-        let headerSize = HEADER_SIZE + lengthInUtf8Bytes(to.join(",") + cc.join(",") + subject);
-        let attachmentsSize = attachments.reduce((acc, val) => acc + val.size, 0);
-        totalSize = headerSize + messageBodySize + attachmentsSize;
+    // Rangement de chaque information dans une variable et calcul des différentes tailles en octets
+    let messageBodySize = lengthInUtf8Bytes(data.isPlainText ? data.plainTextBody : data.body);
+    let to = data.to;
+    let cc = data.cc;
+    let bcc = data.bcc;
+    let subject = data.subject;
+    recipientsCount = to.length + cc.length + bcc.length;
+    let headerSize = HEADER_SIZE + lengthInUtf8Bytes(to.join(",") + cc.join(",") + subject);
+    let attachmentsSize = attachments.reduce((acc, val) => acc + val.size, 0);
+    totalSize = headerSize + messageBodySize + attachmentsSize;
 
-        // Affichage des différentes tailles calculées
-        document.getElementById("header-size").innerHTML = formatBytes(headerSize);
-        document.getElementById("body-size").innerHTML = formatBytes(messageBodySize)
-        document.getElementById("attachments-size").innerHTML = formatBytes(attachmentsSize);
-        document.getElementById("size").innerHTML = formatBytes(totalSize);
+    // Affichage des différentes tailles calculées
+    document.getElementById("header-size").innerHTML = formatBytes(headerSize);
+    document.getElementById("body-size").innerHTML = formatBytes(messageBodySize)
+    document.getElementById("attachments-size").innerHTML = formatBytes(attachmentsSize);
+    document.getElementById("size").innerHTML = formatBytes(totalSize);
 
-        // Calcul des différentes équivalences
-        co2 = recipientsCount === 0 ? totalSize * (CO2 + CO2u) / MO : totalSize * (CO2 + recipientsCount * CO2u) / MO;
-        petrole = co2 / OIL;
-        voiture = co2 / CAR;
-        tgv = co2 / TGV;
-        ampoule = co2 / (BULBW * BULB);
-        respiration = co2 / BREATHING;
+    // Calcul des différentes équivalences
+    co2 = recipientsCount === 0 ? totalSize * (CO2 + CO2u) / MO : totalSize * (CO2 + recipientsCount * CO2u) / MO;
+    petrole = co2 / OIL;
+    voiture = co2 / CAR;
+    tgv = co2 / TGV;
+    ampoule = co2 / (BULBW * BULB);
+    respiration = co2 / BREATHING;
 
-        // Affichage des équivalences
-        document.getElementById("co2").innerHTML = formatGrammes(co2) + (recipientsCount === 0 ? "/<div class='tooltip tooltip-left'>dest.<span class='tooltiptext tooltiptext-left'>destinataire</span></div>" : "");
-        document.getElementById("oil").innerHTML = formatGrammes(petrole) + (recipientsCount === 0 ? "/<div class='tooltip tooltip-left'>dest.<span class='tooltiptext tooltiptext-left'>destinataire</span></div>" : "");
-        document.getElementById("car").innerHTML = formatDistance(voiture) + (recipientsCount === 0 ? "/<div class='tooltip tooltip-left'>dest.<span class='tooltiptext tooltiptext-left'>destinataire</span></div>" : "");
-        document.getElementById("tgv").innerHTML = formatDistance(tgv) + (recipientsCount === 0 ? "/<div class='tooltip tooltip-left'>dest.<span class='tooltiptext tooltiptext-left'>destinataire</span></div>" : "");
-        document.getElementById("bulbw").insertAdjacentHTML('beforeend', BULBW + " W");
-        document.getElementById("bulb").innerHTML = formatTime(ampoule) + (recipientsCount === 0 ? "/<div class='tooltip tooltip-left'>dest.<span class='tooltiptext tooltiptext-left'>destinataire</span></div>" : "");
-        document.getElementById("breathing").innerHTML = formatTime(respiration) + (recipientsCount === 0 ? "/<div class='tooltip tooltip-left'>dest.<span class='tooltiptext tooltiptext-left'>destinataire</span></div>" : "");
-    
-        // Paramétrage des boutons pour ajouter / supprimer la signature
-        document.getElementById("addEqui").onclick = () => {addEquivalences(tabInfo[0].id)};
-        document.getElementById("removeEqui").onclick = () => {removeEquivalences(tabInfo[0].id)};
-    }
+    // Affichage des équivalences
+    document.getElementById("co2").innerHTML = formatGrammes(co2) + (recipientsCount === 0 ? "/<div class='tooltip tooltip-left'>dest.<span class='tooltiptext tooltiptext-left'>destinataire</span></div>" : "");
+    document.getElementById("oil").innerHTML = formatGrammes(petrole) + (recipientsCount === 0 ? "/<div class='tooltip tooltip-left'>dest.<span class='tooltiptext tooltiptext-left'>destinataire</span></div>" : "");
+    document.getElementById("car").innerHTML = formatDistance(voiture) + (recipientsCount === 0 ? "/<div class='tooltip tooltip-left'>dest.<span class='tooltiptext tooltiptext-left'>destinataire</span></div>" : "");
+    document.getElementById("tgv").innerHTML = formatDistance(tgv) + (recipientsCount === 0 ? "/<div class='tooltip tooltip-left'>dest.<span class='tooltiptext tooltiptext-left'>destinataire</span></div>" : "");
+    document.getElementById("bulbw").insertAdjacentHTML('beforeend', BULBW + " W");
+    document.getElementById("bulb").innerHTML = formatTime(ampoule) + (recipientsCount === 0 ? "/<div class='tooltip tooltip-left'>dest.<span class='tooltiptext tooltiptext-left'>destinataire</span></div>" : "");
+    document.getElementById("breathing").innerHTML = formatTime(respiration) + (recipientsCount === 0 ? "/<div class='tooltip tooltip-left'>dest.<span class='tooltiptext tooltiptext-left'>destinataire</span></div>" : "");
+
+    // Paramétrage des boutons pour ajouter / supprimer la signature
+    document.getElementById("addEqui").onclick = () => {addEquivalences(tabInfo[0].id)};
+    document.getElementById("removeEqui").onclick = () => {removeEquivalences(tabInfo[0].id)};
+}
+
+
+const signature = "{0}D'après l'extension {1}, l'envoi de courriel de {2} à {3} destinataire{4} entraîne l'émission indirecte " +
+"de {5} CO{6}e. Cela correspond à la consommation de {7} de pétrole, au parcours de {8} en voiture ou de {9} en TGV," +
+"à l'utilisation d'une ampoule de {10} W pendant {11}, ou encore à la respiration d'un humain pendant {12}.{13}" +
+"Source : base carbone® de l'ADEME (2021), ADEME (2011), Zhang et al. (2011).{14}"
+
+/**
+ * Ajoute une signature au mail
+ * @param {Number} tab Identifiant de l'onglet du mail
+ */
+function addEquivalences(tab) {
+    // Récupération des infos du mail
+    browser.compose.getComposeDetails(tab).then(details => {
+        if (details.isPlainText) {
+            // The message is being composed in plain text mode.
+        /**
+ * Lance une fonction quand le document est chargé
+ * @param {function} fn 
+ */
+ function docReady(fn) {
+    document.addEventListener("DOMContentLoaded", fn);
+}    let body = details.plainTextBody;
+            details.body = null;
+        
+            // Make direct modifications to the message text, and send it back to the editor.
+            body += signature.format(
+                "\n\n", "Estimez votre CO₂ (https://addons.thunderbird.net/fr/thunderbird/addon/estimez-votre-co2/)",
+                formatBytes(totalSize, false), recipientsCount == 0 ? 1 : recipientsCount, recipientsCount <= 1 ? "" : "s",
+                formatGrammes(co2), "₂", formatGrammes(petrole), formatDistance(voiture),
+                formatDistance(tgv), BULBW, formatTime(ampoule), formatTime(respiration), "\n", "");
+
+            details.plainTextBody = body;
+            browser.compose.setComposeDetails(tab, details);
+        } else {
+            // The message is being composed in HTML mode.
+            let body = details.body;
+            details.plainTextBody = null;
+
+            // Make direct modifications to the message text, and send it back to the editor.
+            body += signature.format(
+                "<br><br><small>", "<a href=\"https://addons.thunderbird.net/fr/thunderbird/addon/estimez-votre-co2/\">Estimez votre CO<sub>2</sub></a>",
+                formatBytes(totalSize, false), recipientsCount == 0 ? 1 : recipientsCount, recipientsCount <= 1 ? "" : "s",
+                formatGrammes(co2), "<sub>2</sub>", formatGrammes(petrole), formatDistance(voiture), formatDistance(tgv), BULBW,
+                formatTime(ampoule), formatTime(respiration), "<br>", "</small>");
+        
+            details.body = body;
+            browser.compose.setComposeDetails(tab, details);
+        }
+    });
+}
+
+
+/**
+ * Supprime une signature au mail
+ * @param {Number} tab Identifiant de l'onglet du mail
+ */
+function removeEquivalences(tab){
+    // Récupération des infos du mail
+    browser.compose.getComposeDetails(tab).then(details => {
+        if (details.isPlainText) {
+            // The message is being composed in plain text mode.
+            let body = details.plainTextBody;
+            details.body = null;
+        
+            let indexStart = body.indexOf("\n\nD'après l'extension Estimez votre CO₂");
+            if (indexStart == -1) return;
+            let indexEnd = body.indexOf("Zhang et al. (2011).", indexStart + 1) + 20;
+            // Make direct modifications to the message text, and send it back to the editor.
+            let text = body.substring(indexStart, indexEnd);
+
+            details.plainTextBody = body.replace(text, '');
+            browser.compose.setComposeDetails(tab, details);
+        } else {
+            // The message is being composed in HTML mode.
+            let body = details.body;
+            details.plainTextBody = null;
+        
+            let indexStart = body.indexOf("<br><br><small>D'après l'extension");
+            if (indexStart == -1) return;
+            let indexEnd = body.indexOf("(2011).</small>", indexStart + 1) + 15;
+            // Make direct modifications to the message text, and send it back to the editor.
+            let text = body.substring(indexStart, indexEnd);
+
+            details.body = body.replace(text, '');
+            browser.compose.setComposeDetails(tab, details);
+        }
+    });
+}
+
+
+
+
+// Lorsque le document est chargé
+document.addEventListener("DOMContentLoaded", function () {
+    // Lancement de la promesse pour obtenir les tabs
+    let gettingCurrent = browser.tabs.query({currentWindow: true});
 
     // Lorsque la promesse est terminée, on lance le calcul
     gettingCurrent.then(calculate, onError);
-
-    const signature = "{0}D'après l'extension {1}, l'envoi de courriel de {2} à {3} destinataire{4} entraîne l'émission indirecte " +
-    "de {5} CO{6}e. Cela correspond à la consommation de {7} de pétrole, au parcours de {8} en voiture ou de {9} en TGV," +
-    "à l'utilisation d'une ampoule de {10} W pendant {11}, ou encore à la respiration d'un humain pendant {12}.{13}" +
-    "Source : base carbone® de l'ADEME (2021), ADEME (2011), Zhang et al. (2011).{14}"
-
-    /**
-     * Ajoute une signature au mail
-     * @param {Number} tab Identifiant de l'onglet du mail
-     */
-    function addEquivalences(tab) {
-        // Récupération des infos du mail
-        browser.compose.getComposeDetails(tab).then(details => {
-            if (details.isPlainText) {
-                // The message is being composed in plain text mode.
-                let body = details.plainTextBody;
-                details.body = null;
-            
-                // Make direct modifications to the message text, and send it back to the editor.
-                body += signature.format(
-                    "\n\n", "Estimez votre CO₂ (https://addons.thunderbird.net/fr/thunderbird/addon/estimez-votre-co2/)",
-                    formatBytes(totalSize, false), recipientsCount == 0 ? 1 : recipientsCount, recipientsCount <= 1 ? "" : "s",
-                    formatGrammes(co2), "₂", formatGrammes(petrole), formatDistance(voiture),
-                    formatDistance(tgv), BULBW, formatTime(ampoule), formatTime(respiration), "\n", "");
-
-                details.plainTextBody = body;
-                browser.compose.setComposeDetails(tab, details);
-            } else {
-                // The message is being composed in HTML mode.
-                let body = details.body;
-                details.plainTextBody = null;
-
-                // Make direct modifications to the message text, and send it back to the editor.
-                body += signature.format(
-                    "<br><br><small>", "<a href=\"https://addons.thunderbird.net/fr/thunderbird/addon/estimez-votre-co2/\">Estimez votre CO<sub>2</sub></a>",
-                    formatBytes(totalSize, false), recipientsCount == 0 ? 1 : recipientsCount, recipientsCount <= 1 ? "" : "s",
-                    formatGrammes(co2), "<sub>2</sub>", formatGrammes(petrole), formatDistance(voiture), formatDistance(tgv), BULBW,
-                    formatTime(ampoule), formatTime(respiration), "<br>", "</small>");
-            
-                details.body = body;
-                browser.compose.setComposeDetails(tab, details);
-            }
-        });
-    }
-
-    /**
-     * Supprime une signature au mail
-     * @param {Number} tab Identifiant de l'onglet du mail
-     */
-    function removeEquivalences(tab){
-        // Récupération des infos du mail
-        browser.compose.getComposeDetails(tab).then(details => {
-            if (details.isPlainText) {
-                // The message is being composed in plain text mode.
-                let body = details.plainTextBody;
-                details.body = null;
-            
-                let indexStart = body.indexOf("\n\nD'après l'extension Estimez votre CO₂");
-                if (indexStart == -1) return;
-                let indexEnd = body.indexOf("Zhang et al. (2011).", indexStart + 1) + 20;
-                // Make direct modifications to the message text, and send it back to the editor.
-                let text = body.substring(indexStart, indexEnd);
-
-                details.plainTextBody = body.replace(text, '');
-                browser.compose.setComposeDetails(tab, details);
-            } else {
-                // The message is being composed in HTML mode.
-                let body = details.body;
-                details.plainTextBody = null;
-            
-                let indexStart = body.indexOf("<br><br><small>D'après l'extension");
-                if (indexStart == -1) return;
-                let indexEnd = body.indexOf("(2011).</small>", indexStart + 1) + 15;
-                // Make direct modifications to the message text, and send it back to the editor.
-                let text = body.substring(indexStart, indexEnd);
-
-                details.body = body.replace(text, '');
-                browser.compose.setComposeDetails(tab, details);
-            }
-        });
-    }
 });
