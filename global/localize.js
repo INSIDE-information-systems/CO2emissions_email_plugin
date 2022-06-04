0589e7f14898894d046eec556cc40f49e2240126
Harfeur <maxou3153@gmail.com>
2022-06-04 15:59:12 +0200

    Début de traduction HTML + script

---
+++ global/localize.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('[data-locale]').forEach(elem => {
+        elem.innerText = browser.i18n.getMessage(elem.dataset.locale)
+    });
+});
